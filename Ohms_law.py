a2e0de815506ed8308a8b689fc5b3b0a5a456a58
rofiat101 <buliaminuyetunde@gmail.com>
2025-08-07 19:53:04 +0100

    Create Ohms_law.py

---
+++ Ohms_law.py
@@ -0,0 +1,4 @@
+current = float(input("Enter the current (in A): "))
+resistance = float(input("Enter the resistance (in ohms): "))
+voltage = current * resistance
+print(f"The voltage is: {voltage} volts")
