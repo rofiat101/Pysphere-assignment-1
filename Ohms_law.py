9b7ef3d4d3a070e9207b1777849b840cb42aab84
rofiat101 <buliaminuyetunde@gmail.com>
2025-08-09 03:51:41 +0100

    Update Ohms_law.py

--- Ohms_law.py
+++ Ohms_law.py
@@ -2,13 +2,13 @@
 # V = I * R
 
 # Get current (in Amperes) from user
-current = float(input("Enter the current (in A):5 "))
+current = float(input("Enter the current (in A): "))
 
 # Get resistance (in Ohms) from user
-resistance = float(input("Enter the resistance (in ohms): 10"))
+resistance = float(input("Enter the resistance (in ohms): "))
 
 # Calculate voltage
 voltage = current * resistance
 
 # Display result
-print(f"The voltage is: 50 {voltage} volts")
+print(f"The voltage is: {voltage} volts")
